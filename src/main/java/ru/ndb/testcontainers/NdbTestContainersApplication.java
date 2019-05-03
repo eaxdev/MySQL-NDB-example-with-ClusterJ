f234b6ad3017248aa5bbe5b1238857729abab4eb
pi <romask17@ya.ru>
2019-05-03 22:01:07 +0300

    initial

---
+++ src/main/java/ru/ndb/testcontainers/NdbTestContainersApplication.java
@@ -0,0 +1,13 @@
+package ru.ndb.testcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdbTestContainersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NdbTestContainersApplication.class, args);
+    }
+
+}
