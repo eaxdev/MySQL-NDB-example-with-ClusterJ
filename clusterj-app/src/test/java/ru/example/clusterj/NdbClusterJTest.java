37de4c85b8b87f035f19dcd5cf895c7b8fb06ef5
eaxdev <eaxdev@ya.ru>
2019-10-19 23:07:28 +0300

    add query builder test

--- clusterj-app/src/test/java/ru/example/clusterj/NdbClusterJTest.java
+++ clusterj-app/src/test/java/ru/example/clusterj/NdbClusterJTest.java
@@ -1,7 +1,12 @@
 package ru.example.clusterj;
 
+import com.mysql.clusterj.Query;
 import com.mysql.clusterj.Session;
 import com.mysql.clusterj.SessionFactory;
+import com.mysql.clusterj.query.PredicateOperand;
+import com.mysql.clusterj.query.QueryBuilder;
+import com.mysql.clusterj.query.QueryDomainType;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,8 +18,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.example.clusterj.junit5.EnableMySQLClusterContainer;
 import ru.example.clusterj.model.User;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -54,5 +63,59 @@ class NdbClusterJTest {
                 () -> assertEquals(userFromDb.getId(), 1),
                 () -> assertEquals(userFromDb.getFirstName(), "John"),
                 () -> assertEquals(userFromDb.getLastName(), "Jonson"));
+    }
+
+    @Test
+    void queryBuilderTest() {
+        User newUser1 = session.newInstance(User.class);
+        newUser1.setId(1);
+        newUser1.setFirstName("John");
+        newUser1.setLastName("Jonson");
+
+        User newUser2 = session.newInstance(User.class);
+        newUser2.setId(2);
+        newUser2.setFirstName("Alex");
+        newUser2.setLastName("Jonson");
+
+        session.persist(newUser1);
+        session.persist(newUser2);
+
+        QueryBuilder builder = session.getQueryBuilder();
+        QueryDomainType<User> userQueryDomainType = builder.createQueryDefinition(User.class);
+        // parameter
+        PredicateOperand propertyIdParam = userQueryDomainType.param("lastName");
+        // property
+        PredicateOperand propertyEntityId = userQueryDomainType.get("lastName");
+        userQueryDomainType.where(propertyEntityId.equal(propertyIdParam));
+
+        Query<User> query = session.createQuery(userQueryDomainType);
+        query.setParameter("lastName", "Jonson");
+        List<User> foundEntities = query.getResultList();
+        Optional<User> firstUser = foundEntities.stream().filter(u -> u.getId() == 1).findFirst();
+        Optional<User> secondUser = foundEntities.stream().filter(u -> u.getId() == 2).findFirst();
+
+        assertAll(
+                () -> assertEquals(foundEntities.size(), 2),
+                () -> assertTrue(firstUser.isPresent()),
+                () -> assertTrue(secondUser.isPresent()),
+                () -> assertThat(firstUser.get(),
+                        allOf(
+                                hasProperty("firstName", equalTo("John")),
+                                hasProperty("lastName", equalTo("Jonson"))
+                        )
+                ),
+                () -> assertThat(secondUser.get(),
+                        allOf(
+                                hasProperty("firstName", equalTo("Alex")),
+                                hasProperty("lastName", equalTo("Jonson"))
+                        )
+                )
+        );
+    }
+
+    @AfterEach
+    void tearDown() {
+        session.deletePersistentAll(User.class);
+        session.close();
     }
 }
